06aedcd581f21e9f5b98dd8e186b30f170b4a7d2
Maftei Albert-Alexandru <maftei.albert.alexandru@gmail.com>
2022-11-21 18:41:25 +0200

    V0.0

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SliderSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystem/SliderSubsystem.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.subsystem;
 
-import static org.firstinspires.ftc.teamcode.util.DriveConstants.kV;
 
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
@@ -19,53 +18,82 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
-import org.apache.commons.math3.geometry.euclidean.twod.Line;
-import org.firstinspires.ftc.teamcode.drive.opmode.DriveVelocityPIDTuner;
 import org.firstinspires.ftc.teamcode.hardware.customHardware.SmartMotor;
 import org.firstinspires.ftc.teamcode.hardware.customHardware.SmartMotorEx;
-import org.firstinspires.ftc.teamcode.hardware.customHardware.ToggleButton;
 
-import java.util.List;
 
 
 @Config
 public class SliderSubsystem extends SmartSubsystem {
 
     public SmartMotorEx slider;
-    public static double target = 0;
-    public static double tolerance = 1000;
+
+
+    public  int target = 0;
+    public static double tolerance = 30;
     public static double upwardCoefficient = 0.04;
     public static double downwardCoefficient =0.04;
     public static double pow = 0.1;
+
     public static int groundPos =200;
     public static int lowPos =400;
     public static int midPos = 570;
     public static int highPos = 1550;
-    public static double downForce = 0;
-    public static double upForce = 0;
-    public static double ff = 0;
 
-    public static boolean telemetryOn = false;
+    public static int safePos = 200;
+    public static int loadPos= 50;
+    public static int clearPos = 300;
 
-    enum Mode {
-        AUTO_MODE,
-        MANUAL_MODE
+
+    public boolean isClear()
+    {
+        return slider.getCurrentPosition() >= safePos;
+    }
+    public void goToClear()
+    {
+        target=clearPos;
+        slider.setTargetPosition(target);
     }
 
-    Mode mode = Mode.AUTO_MODE;
+    public static boolean telemetryOn = false;
+    public void goToTake()
+    {
+        target=loadPos;
+        slider.setTargetPosition(target);
+    }
+    public void goToPosition(int position)
+    {
+        target=position;
+        slider.setTargetPosition(position);
+    }
+    public Long lastTime=null;
+    public static float clamp(float val, float min, float max) {
+        return Math.max(min, Math.min(max, val));
+    }
+    public static float step = 10;
+    public void move(float offset)
+    {
+        Long time = System.currentTimeMillis();
+        if(lastTime==null) lastTime=time;
 
+
+        target= (int) clamp(target+step*offset*(time-lastTime)/1000.0f, 0, highPos);
+        slider.setTargetPosition(target);
+    }
     @Override
     public void run(SubsystemData data) throws InterruptedException {
+        if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_UP)) target = highPos;
+        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) target = midPos;
+        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) target = groundPos;
+        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) target = lowPos;
 
-        switch (mode) {
-            case AUTO_MODE:
-               SliderAutoMode(data);
-                break;
-            case MANUAL_MODE:
-                SliderManualMode(data);
-                break;
-        }
+        if(target>slider.getCurrentPosition()) slider.setPositionCoefficient(upwardCoefficient);
+        else slider.setPositionCoefficient(downwardCoefficient);
+        slider.setPositionTolerance(tolerance);
 
+        slider.set(pow);
+        slider.setTargetPosition((int)target);
+        slider.setTargetPosition(target);
 
         if(telemetryOn){
             opMode.telemetry.addData("pos", slider.getCurrentPosition());
@@ -82,25 +110,6 @@ public class SliderSubsystem extends SmartSubsystem {
         slider.setRunMode(SmartMotor.RunMode.PositionControl);
         slider.setZeroPowerBehavior(SmartMotor.ZeroPowerBehavior.FLOAT);
         slider.setInverted(true);
-    }
-
-    void SliderAutoMode(SubsystemData data){
-        slider.setRunMode(SmartMotor.RunMode.PositionControl);
-
-        if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_UP)) target = highPos;
-        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_LEFT)) target = midPos;
-        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_DOWN)) target = groundPos;
-        else if(data.operatorGamepad.wasJustPressed(GamepadKeys.Button.DPAD_RIGHT)) target = lowPos;
-
-        if(target>slider.getCurrentPosition()) slider.setPositionCoefficient(upwardCoefficient);
-        else slider.setPositionCoefficient(downwardCoefficient);
-        slider.setPositionTolerance(tolerance);
-
-        slider.set(pow);
-        slider.setTargetPosition((int)target);
-    }
-
-    void SliderManualMode(SubsystemData data){
-        slider.setRunMode(SmartMotor.RunMode.RawPower);
+        target=0;
     }
 }
